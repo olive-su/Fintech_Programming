2d41e34922c406ad911585fcf2d97362bf87ec2d
olive-su <1466su@naver.com>
2021-05-27 14:00:15 +0900

    Token, userSeqNum/ 페이지 전환

--- fintech/src/pages/AuthResultPage.js
+++ fintech/src/pages/AuthResultPage.js
@@ -37,6 +37,9 @@ const AuthResultPage = () => {
       console.log(response.data);
       setaccessToken(response.data.access_token);
       localStorage.setItem("accessToken", response.data.access_token);
+      localStorage.setItem("userSeqNum", response.data.user_seq_no);
+      window.opener.location.href = "/list";
+      window.close();
     });
   };
 
